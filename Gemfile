95d831e2f34d64c8cbb345a4531579a05da62c95
Aaron Riggs <riggs.aaron@gmail.com>
2015-10-09 18:26:13 -0700

    reverting in order to publish my commits to the new week1 branch

--- Gemfile
+++ Gemfile
@@ -25,7 +25,6 @@ end
 
 group :test do
   gem 'database_cleaner', '1.2.0'
-  gem 'shoulda-matchers'
 end
 
 group :production do
--- app/models/video.rb
+++ app/models/video.rb
@@ -7,9 +7,4 @@ class Video < ActiveRecord::Base
   validates :large_cover_url, presence: true
 
   sluggable_column :title
-
-  def self.search_by_title(search)
-    return [] if search.blank?
-    where("lower(title) LIKE ?", "%#{search.downcase}%").order('created_at DESC')
-  end
 end

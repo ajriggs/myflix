339a762fd9e90f7af812d1120dd8b4903e08487d
Aaron Riggs <riggs.aaron@gmail.com>
2016-03-26 14:29:56 -0700

    updates to production config to support mailgun from heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,11 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:               'myflix-riggs.heroku.com',
+    authentication:       :plain,
+    }
 end

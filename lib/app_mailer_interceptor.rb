340b4af7f8b63205af21c41789106fe6bb3e6cbe
Aaron Riggs <riggs.aaron@gmail.com>
2016-03-28 13:29:33 -0700

    updates to mailer interceptor

--- lib/app_mailer_interceptor.rb
+++ lib/app_mailer_interceptor.rb
@@ -3,6 +3,6 @@ class AppMailerInterceptor
    ADMIN_EMAIL = 'riggs.aaron@gmail.com'
 
    message.subject = "STAGING TEST #{message.subject}"
-   message.to = ADMIN_EMAIL
+   message.to ADMIN_EMAIL
  end
 end

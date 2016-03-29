fbc4afe9a47b1679e4c973ec0aa0df56ad977da1
Aaron Riggs <riggs.aaron@gmail.com>
2016-03-28 17:34:26 -0700

    update to email interceptor

--- lib/app_email_interceptor.rb
+++ lib/app_email_interceptor.rb
@@ -2,7 +2,7 @@ class AppEmailInterceptor
  def self.delivering_email(message)
    admin_email = 'riggs.aaron@gmail.com'
 
-   message.subject = "STAGING TEST #{message.subject}"
+   message.subject = "STAGING TEST: #{message.subject}"
    message.to admin_email
  end
 end
